fcbc1d1b16a79573cc908f1eb914585f8c6bbd31
Kalle Kankare <kalle.kankare@iki.fi>
2017-07-05 21:30:29 +0300

    web: adapt web ui to the new backend

--- web/controller.js
+++ web/controller.js
@@ -1,48 +1,50 @@
 var paperlessApp = angular.module('PaperlessApp',["ngResource"]);
 
 paperlessApp.config(function($locationProvider) {
-    $locationProvider.html5Mode(true)
-})
+    $locationProvider.html5Mode(true);
+});
 
 paperlessApp.controller("PaperlessCtrl", ["$scope", "$location", "$resource", function($scope, $location, $resource) {
-    var Image = $resource("/images/:id", {id: '@id'}, {})
+    var Image = $resource("/api/v1/image/:id", {id: '@id'}, {});
 
     var showError = function(error) {
-	$scope.error = error.data.error
-    }
+	$scope.error = error.data.error;
+    };
 
     $scope.list = function(searchparam) {
 	Image.get({q: searchparam},function(data) {
-	    $scope.images = data['Images'];
-	    $scope.img_offset = data['Offset'];
-	    $scope.img_count = data['Count'];
-	    $scope.img_limit = data['Limit'];
-	    $scope.error = null
-
+          console.log(data);
+          $scope.images = data.data;
+            // $scope.img_offset = data['Offset'];
+	    // // $scope.img_count = data['Count'];
+	    // $scope.img_count = data.length;
+	    // $scope.img_limit = data['Limit'];
+          if (data.status != 'success') {
+	    $scope.error = data.status;
+          }
 	}, showError);
     };
 
     $scope.search = function() {
-	$scope.images = null
-	$scope.list($scope.searchstring)
-	$location.search('q',$scope.searchstring)
-    }
+	$scope.images = null;
+	$scope.list($scope.searchstring);
+	$location.search('q',$scope.searchstring);
+    };
 
     $scope.ifTrue = function(cond, appendix) {
 	return cond ? appendix : '';
-    }
+    };
 
     $scope.toDate = function(dateint) {
 	if(dateint <= 0) {
 	    return "";
 	}
 	return new Date(dateint * 1000);
-    }
+    };
 
     $scope.toggleProcessed = function(img) {
 	img.showImg = !img.showImg;
-	img.procURL = "static/" + img.Fileid + "processed.jpg";
-    }
+    };
 
     // Initial load
     $scope.view = 'list';
