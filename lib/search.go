4acefe97f44d06e65a4eeb88d6f3899a1dcd09b8
Kalle Kankare <kalle.kankare@iki.fi>
2017-08-20 16:22:59 +0300

    search: WIP

--- lib/search.go
+++ lib/search.go
@@ -1,9 +1,16 @@
 package paperless
 
+import (
+	"fmt"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
 type TokenType int
 
 const (
-	eof rune = -1
+	eof      rune      = -1
 	TokError TokenType = iota
 	TokCanceled
 	TokEOF
@@ -11,6 +18,7 @@ const (
 	TokAnd
 	TokOr
 	TokNot
+	TokQuote
 	TokString
 	TokParOpen
 	TokParClose
@@ -20,7 +28,7 @@ type Query struct {
 }
 
 type Token struct {
-	Type TokenType
+	Type  TokenType
 	Value string
 	Pos   int
 }
@@ -33,6 +41,7 @@ type lexer struct {
 	pos   int
 	width int
 
+	states      []stateFunc
 	initialized bool
 
 	tokens chan Token
@@ -68,6 +77,8 @@ func (l *lexer) Init(input string) {
 	l.input = input
 	l.start = 0
 	l.pos = 0
+	l.width = 0
+	l.states = nil
 	l.tokens = make(chan Token)
 	// l.cancel = make(chan bool)
 	// l.wg = sync.WaitGroup{}
@@ -106,22 +117,121 @@ func (l *lexer) next() rune {
 		return eof
 	}
 
-	return 0
+	var ret rune
+	ret, l.width = utf8.DecodeRuneInString(l.input[l.pos:])
+	l.pos += l.width
+	return ret
 }
 
-func lexText(l *lexer) stateFunc {
+func (l *lexer) rewind() {
+	l.pos -= l.width
+}
+
+func (l *lexer) ignore() {
+	l.start = l.pos
+}
+
+func (l *lexer) peek() rune {
+	ret := l.next()
+	l.rewind()
+	return ret
+}
+
+func (l *lexer) hasPrefix(prefix string) bool {
+	if strings.HasPrefix(l.input[l.pos:], prefix) {
+		// l.pos += len(prefix)
+		return true
+	}
+	return false
+}
+
+func (l *lexer) acceptRune(charclass string) bool {
+	// TODO
+	return false
+}
+
+func (l *lexer) errorf(format string, args ...interface{}) stateFunc {
+	l.tokens <- Token{TokError, fmt.Sprintf(format, args...), l.pos}
 	return nil
 }
 
+func (l *lexer) push(s stateFunc) {
+	l.states = append(l.states, s)
+}
+
+func (l *lexer) pop() stateFunc {
+	if l.states == nil || len(l.states) == 0 {
+		return l.errorf("Internal error: Popping an empty stack")
+	}
+
+	ret := l.states[len(l.states)-1]
+	l.states = l.states[:len(l.states)-1]
+	return ret
+}
+
+func lexTop(l *lexer) stateFunc {
+	for {
+		r := l.next()
+		if r == eof {
+			break
+		}
+		switch {
+		case unicode.IsSpace(r):
+			l.ignore()
+		case r == '"':
+			l.push(lexTop)
+			return lexQuoted
+		}
+	}
+
+	if l.pos > l.start {
+		l.errorf("Internal error: Following not handled: %s", l.input[l.start:l.pos])
+	}
+
+	l.emit(TokEOF)
+	return nil
+}
+
+func lexString(l *lexer) stateFunc {
+	// r := l.next()
+
+	if l.hasPrefix("AND ") {
+		l.emit(TokString)
+		
+	}
+
+	r := l.next()
+	switch r {
+	case '"':
+		l.push(lexString)
+		return lexQuoted
+	}
+
+	return lexString
+}
+
+func lexQuoted(l *lexer) stateFunc {
+	r := l.next()
+
+	switch {
+	case r == '"':
+		l.emit(TokString)
+		return l.pop()
+	case r == eof:
+		return l.errorf("Unbalanced quotes")
+	}
+	return lexQuoted
+}
+
 func (l *lexer) run() {
-	defer func() {
-		// close(l.tokens)
-		// close(l.cancel)
-		// l.clear("")
-		// l.initialized = false
-		// l.wg.Done()
-	}()
-	for s := lexText; s != nil; {
+	// defer func() {
+	// close(l.tokens)
+	// close(l.cancel)
+	// l.clear("")
+	// l.initialized = false
+	// l.wg.Done()
+	// }()
+	for s := lexTop; s != nil; {
 		s = s(l)
 	}
 	close(l.tokens)
--- lib/search_test.go
+++ lib/search_test.go
@@ -1,18 +1,41 @@
 package paperless
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+)
 
 func Test_lexer_Init(t *testing.T) {
 	tests := []struct {
 		name   string
-		input string
+		input  string
+		output []TokenType
 	}{
-	// TODO: Add test cases.
+		{"Empty search", "", []TokenType{TokEOF}},
+		{"Text search", "some text", []TokenType{TokString, TokEOF}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			l := &lexer{}
 			l.Init(tt.input)
+
+			var result []TokenType
+			var tokens []Token
+			for {
+				t := l.NextToken()
+				tokens = append(tokens, t)
+				result = append(result, t.Type)
+				if t.Type == TokEOF {
+					break
+				}
+			}
+
+			if !structEquals(tt.output, result) {
+				t.Error("List of tokens differs:\n", diffStr(tt.output, result),
+					"Outputted list of tokens:\n", spew.Sdump(tokens))
+			}
+			l.Deinit()
 		})
 	}
 }
