2eab2276722c46415f067d70c66e3c3adf86f19d
Kalle Kankare <kalle.kankare@iki.fi>
2020-05-30 10:15:27 +0300

    frontend: Fixed v-for key warnings

--- web/paperless-frontend/src/App.vue
+++ web/paperless-frontend/src/App.vue
@@ -76,7 +76,7 @@
             </div>
           </div>
           <div class="col-md-10">
-            <span v-for="tag in info.image.Tags" :key="tag" class="badge">{{tag.Name}}</span>
+            <span v-for="tag in info.image.Tags" :key="tag.Id" class="badge">{{tag.Name}}</span>
             <pre class="pre-scrollable pap-text">{{info.showLog ? info.image.ProcessLog : info.image.Text}}</pre>
 
             <!-- Debugging -->
@@ -112,7 +112,7 @@
     <div class="container pap-info panel">
       <div class="text-center">
         matches: {{matches}} Results per page: {{images.length}} <br>
-        <button type="button" v-for="tag in tags" :key="tag" class="badge" style="margin-right:2pt" @click="doTagSearch(tag.Name)">
+        <button type="button" v-for="tag in tags" :key="tag.Id" class="badge" style="margin-right:2pt" @click="doTagSearch(tag.Name)">
           {{tag.Name}}
         </button>
         <div class="alert alert-danger" v-for="err in errors" :key="err">
@@ -143,7 +143,7 @@
     <div class="container pap-body">
       <div class="list-group">
         <a href="javascript:void(0)" @click.prevent.stop="doShowInfo(image)"
-           v-for="image in images" :key="image" class="list-group-item pap-item">
+           v-for="image in images" :key="image.Id" class="list-group-item pap-item">
           <div class="media">
             <div class="media-left">
               <img class="media-object img-rounded" :src="imgbase + image.ThumbImg"
@@ -151,7 +151,7 @@
             </div>
             <div class="media-body">
               <!-- <h4 class="media-heading"> -->
-                <span v-for="tag in image.Tags" :key="tag" class="badge">{{tag.Name}}</span>
+                <span v-for="tag in image.Tags" :key="tag.Id" class="badge">{{tag.Name}}</span>
                 <!-- dirps -->
                 <!-- </h4> -->
                 <p>
